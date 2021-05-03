498847eb445668095e4fb213a6944f4131240e9d
dongminlee94 <kid33629@gmail.com>
2021-05-03 10:26:44 +0900

    Change codes from develop branch to develop-pearl branch

--- pearl/algorithm/__init__.py
+++
@@ -1,5 +0,0 @@
-from algorithm.sac import SAC
-# from algorithm.metalearner import MetaLearner
-# from algorithm.sampler import InPlacePathSampler
-# from algorithm.buffers import MultiTaskReplayBuffer, SimpleReplayBuffer
-from algorithm.networks import MLP, FlattenMLP, MLPEncoder, TanhGaussianPolicy
