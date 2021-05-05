2506c07500107e3f7f133a869723d7d49e70f39a
dongminlee94 <kid33629@gmail.com>
2021-05-05 09:32:28 +0900

    Refactored sac

--- pearl/algorithm/sac.py
+++ pearl/algorithm/sac.py
@@ -78,7 +78,7 @@ class SAC(object):
     def get_action(self, obs, deterministic=False):
         ''' Sample action from the policy, conditioned on the task embedding '''
         z = self.encoder.z
-        obs = torch.from_numpy(obs).float()
+        obs = torch.from_numpy(obs[None]).float()
         print(z.shape)
         print(obs.shape)
         inputs = torch.cat([obs, z], dim=-1)
