97d34c03b932aebefdcb9b7f37cf619130e35f35
dongminlee94 <kid33629@gmail.com>
2021-05-10 23:12:08 +0900

    Update train model function

--- pearl/envs/__init__.py
+++ pearl/envs/__init__.py
@@ -27,7 +27,7 @@ def register_env(name):
 for file in os.listdir(os.path.dirname(__file__)):
     if file.endswith('.py') and not file.startswith('_'):
         module = file[:file.find('.py')]
-        importlib.import_module('pybullet_envs.' + module)
+        importlib.import_module('envs.' + module)
 
 # ------------bullet-------------
 
