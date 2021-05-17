a4ad43d73b487e6a6c967e3b2f49a4b05f359d5d
dongminlee94 <kid33629@gmail.com>
2021-05-17 23:56:46 +0900

    Update train model

---
+++ pearl/envs/__init__.py
@@ -0,0 +1,29 @@
+import os
+import importlib
+
+import gym
+from gym.envs.registration import registry, make, spec
+
+envs = {}
+
+def register(id, *args, **kvargs):
+  if id in registry.env_specs:
+    return
+  else:
+    return gym.envs.registration.register(id, *args, **kvargs)
+
+def register_env(name):
+    def register_env_fn(fn):
+        if name in envs:
+            raise ValueError("Cannot register duplicate env {}".format(name))
+        if not callable(fn):
+            raise TypeError("env {} must be callable".format(name))
+        envs[name] = fn
+        return fn
+    return register_env_fn
+
+# automatically import any envs in the envs/ directory
+for file in os.listdir(os.path.dirname(__file__)):
+    if file.endswith('.py') and not file.startswith('_'):
+        module = file[:file.find('.py')]
+        importlib.import_module('envs.' + module)
