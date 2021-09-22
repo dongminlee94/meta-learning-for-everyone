2871f025f24d78ae8bc5ba10b5a91dc3ca8aa80b
Seunghyun Lee <shlee21@postech.ac.kr>
2021-09-22 13:14:35 +0900

    Replace linear-feature baseline to value network and Add a variable to store old_policy

--- src/maml/algorithm/ppo.py
+++ src/maml/algorithm/ppo.py
@@ -2,10 +2,11 @@
 Proximal Policy Optimization algorithm implementation for training
 """
 
+import copy
+
 import torch
 
-from src.maml.algorithm.buffer import Buffer
-from src.maml.algorithm.networks import LinearValue, TanhGaussianPolicy
+from src.maml.algorithm.networks import MLP, GaussianPolicy
 
 
 class PPO:  # pylint: disable=too-many-instance-attributes
@@ -15,7 +16,8 @@ class PPO:  # pylint: disable=too-many-instance-attributes
         self,
         observ_dim,
         action_dim,
-        hidden_dim,
+        policy_hidden_dim,
+        vf_hidden_dim,
         device,
         gamma=0.99,
         lamda=0.97,
@@ -26,59 +28,41 @@ class PPO:  # pylint: disable=too-many-instance-attributes
         self.gamma = gamma
         self.lamda = lamda
         self.clip_param = config["clip_param"]
-        self.vf_clip_param = config["vf_clip_param"]
 
-        self.policy = TanhGaussianPolicy(
-            input_dim=observ_dim, output_dim=action_dim, hidden_dim=hidden_dim
+        self.policy = GaussianPolicy(
+            input_dim=observ_dim, output_dim=action_dim, hidden_dim=policy_hidden_dim
         ).to(device)
+        self.old_policy = copy.deepcopy(self.policy)
 
-        self.baseline = LinearValue(
-            input_dim=observ_dim,
-        ).to(device)
+        self.vf = MLP(input_dim=observ_dim, output_dim=1, hidden_dim=vf_hidden_dim).to(device)
 
-    @staticmethod
-    def get_action(policy, obs, device):
-        """Get an action from the policy"""
-        action, log_prob = policy(torch.Tensor(obs).to(device))
-        if log_prob:
-            log_prob = log_prob.detach().cpu().numpy()
-        return action.detach().cpu().numpy(), log_prob
+        self.vf_optimizer = torch.optim.Adam(
+            list(self.vf.parameters()),
+            lr=config["vf_learning_rate"],
+        )
+        self.initial_vf_state = self.vf.state_dict()
 
-    def get_value(self, obs):
-        """Get an value from the value network"""
-        value = self.baseline(torch.Tensor(obs).to(self.device))
-        return value.detach().cpu().numpy()
+    def reset_vf(self):
+        """Reset value fuction"""
+        self.vf.load_state_dict(self.initial_vf_state)
 
     # pylint: disable=too-many-locals
-    def compute_losses(self, policy, batch, a2c_loss=False, clip_loss=True):
+    def compute_loss(self, policy, batch, valid=False):
         """Compute policy losses according to PPO algorithm"""
         obs_batch = batch["obs"]
         action_batch = batch["actions"]
-        return_batch = batch["returns"]
-        reward_batch = batch["rewards"]
-        done_batch = batch["dones"]
-        log_prob_batch = batch["log_probs"]
+        advant_batch = batch["advants"]
 
-        # Compute advantage with standard linear feature baseline (Daun el al. 2016)
-        self.baseline.fit(obs_batch, return_batch)
-        value_batch = self.baseline(obs_batch).detach()
-        advant_batch = Buffer.compute_gae(reward_batch, done_batch, value_batch).to(self.device)
-
-        # Compute Policy loss
-        new_log_prob_batch = policy.get_log_prob(obs_batch, action_batch)
-        if a2c_loss:
-            policy_loss = new_log_prob_batch * advant_batch
-            policy_loss = -policy_loss.mean()
+        # Policy loss
+        if valid:
+            old_log_prob_batch = batch["log_probs"]
         else:
-            ratio = torch.exp(new_log_prob_batch - log_prob_batch)
-            surr_loss = ratio * advant_batch
-            if clip_loss:
-                clipped_loss = (
-                    torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advant_batch
-                )
-                policy_loss = -torch.min(surr_loss, clipped_loss).mean()
-            else:
-                policy_loss = -surr_loss.mean()
+            old_log_prob_batch = self.old_policy.get_log_prob(obs_batch, action_batch)
+        new_log_prob_batch = policy.get_log_prob(obs_batch, action_batch)
 
-        # print(policy_loss)
+        ratio = torch.exp(new_log_prob_batch.view(-1, 1) - old_log_prob_batch)
+        policy_loss = ratio * advant_batch
+        clipped_loss = torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advant_batch
+        policy_loss = -torch.min(policy_loss, clipped_loss).mean()
+
         return policy_loss
