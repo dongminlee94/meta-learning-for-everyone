ea5b9966e8f44c910c3df984a71ca2a828a91df2
Seunghyun Lee <shlee21@postech.ac.kr>
2021-09-27 10:39:25 +0900

    Remove numpy conversion from cuda tensor

--- src/maml/algorithm/ppo.py
+++ src/maml/algorithm/ppo.py
@@ -74,7 +74,7 @@ class PPO:  # pylint: disable=too-many-instance-attributes
         running_advant = 0
 
         self.train_value_network(batch)
-        values_batch = self.vf(obs_batch).detach().cpu().numpy()
+        values_batch = self.vf(obs_batch).detach().cpu()
 
         for t in reversed(range(len(rewards_batch))):
             # Compute GAE
